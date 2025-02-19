7e5270f700ab73bd5f60ca07e9338afa9f5bc9db
aparties <fiestas.armando@gmail.com>
2025-02-19 17:38:38 -0500

    Manejo de bienvenida en primera visita

--- codigo/logueado.js
+++ codigo/logueado.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loggedIn = localStorage.getItem('loggedIn') === 'true';
     const userEmail = localStorage.getItem('userEmail');
+    const isFirstVisit = localStorage.getItem('isFirstVisit') === 'true';  // Verifica si es la primera vez que visita
 
     if (loggedIn) {
         document.getElementById('userEmailHeader').textContent = userEmail;
         document.getElementById('userInfo').style.display = 'inline'; // Muestra el correo y el botón de cerrar sesión
-        alert(`Bienvenido, ${userEmail}!`);
-    
+        
+        // Solo mostrar el mensaje de bienvenida si es la primera vez que el usuario visita después de iniciar sesión
+        if (isFirstVisit) {
+            alert(`Bienvenido, ${userEmail}!`);
+            localStorage.setItem('isFirstVisit', 'false'); // Marca que ya no es la primera visita
+        }
     } else {
         document.getElementById('bton-login').style.display = 'inline'; // Muestra el enlace de login
-        alert('¡Bienvenido! a tu Tienda favorita.');
     }
 
-});
-
-// Manejo del boton de cerrar sesión
-const logoutButton = document.getElementById('logoutButton');
-logoutButton.addEventListener('click', () => {
-    // Eliminar datos de localStorage
-    localStorage.removeItem('loggedIn');
-    localStorage.removeItem('userEmail');
-    // Redirigir a la página de login
-    window.location.href = 'login.html';
+    // Manejo del botón de cerrar sesión
+    const logoutButton = document.getElementById('logoutButton');
+    if (logoutButton) {
+        logoutButton.addEventListener('click', () => {
+            // Eliminar datos de localStorage
+            localStorage.removeItem('loggedIn');
+            localStorage.removeItem('userEmail');
+            localStorage.removeItem('isFirstVisit');  // Eliminar el flag de la primera visita
+            // Redirigir a la página de login
+            window.location.href = 'login.html';
+        });
+    }
 });
