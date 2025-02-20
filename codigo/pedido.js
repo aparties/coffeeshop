e411ff924c29474ddaa6223472d706d31c7d3c25
Jaime Suarez <jaisua8@gmail.com>
2025-02-19 23:41:13 -0500

    actualizacion de referencia

--- codigo/pedido.js
+++ codigo/pedido.js
@@ -2,17 +2,15 @@ const li= document.querySelectorAll('.li')
 const bloque=document.querySelectorAll('.bloque')
 
 //cuando hacemos click en un li
-    //todos .li quitar la clase activo
-    //todos .bloque quitar la clases activo
-    //.li con la posicion se añadimos la clase activo
-    //.bloque con la posicion se añadimos la clase activo.
+    //quita de todos los .li y .bloque la clase activo
+    //.li y .bloque con la posicion se añadimos la clase activo
+
 li.forEach((cadaLi , i)=>{
     li[i].addEventListener('click',()=>{
         li.forEach((cadaLi,i)=>{
             li[i].classList.remove('activo')
             bloque[i].classList.remove('activo')
         })
-
         li[i].classList.add('activo')
         bloque[i].classList.add('activo')
     })
@@ -21,11 +19,6 @@ li.forEach((cadaLi , i)=>{
 const li2= document.querySelectorAll('.li2')
 const bloque2=document.querySelectorAll('.bloque2')
 
-//cuando hacemos click en un li
-    //todos .li quitar la clase activo
-    //todos .bloque quitar la clases activo
-    //.li con la posicion se añadimos la clase activo
-    //.bloque con la posicion se añadimos la clase activo.
 li2.forEach((cadaLi , i)=>{
     li2[i].addEventListener('click',()=>{
         li2.forEach((cadaLi,i)=>{
