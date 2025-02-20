93182f411e1c4920147c0e6a09fc820ead39d490
Jaime Suarez <jaisua8@gmail.com>
2025-02-20 01:43:11 -0500

    direccionamiento a register -pedido

--- codigo/pedido.js
+++ codigo/pedido.js
@@ -9,17 +9,17 @@ const bloque2=document.querySelectorAll('.bloque2')
 //Funcion para cuando hacemos click en un li
     //quita de todos los .li y .bloque la clase activo
     //.li y .bloque con la posicion se añadimos la clase activo
-const eliminarClase = ( bloque, nameClass) => {
-    li.forEach((element,i)=>{
-        element.classList.remove(nameClass);
-        bloque[i].classList.remove(nameClass);
+const eliminarClase = ( pestaña, ventana, clase) => {
+    pestaña.forEach((element,i)=>{
+        element.classList.remove(clase);
+        ventana[i].classList.remove(clase);
     })
 }
 
 function recorrerMenus(li, bloque, nameClass){
     li.forEach((element , i)=>{
         element.addEventListener('click',()=>{
-            eliminarClase(bloque, nameClass);
+            eliminarClase(li, bloque, nameClass);
             element.classList.add(nameClass);
             bloque[i].classList.add(nameClass);
         })
@@ -33,8 +33,16 @@ recorrerMenus(li2, bloque2, "activo2")
 document.addEventListener("DOMContentLoaded", function () {
     const loggedIn = localStorage.getItem('loggedIn') === 'true';
     if (loggedIn) {
-        eliminarClase('activo');
+        eliminarClase(li, bloque,'activo');
         li[1].classList.add("activo");
         bloque[1].classList.add("activo");
     }
 })
+
+
+
+function registrarEmail(){
+    const email = document.getElementById('tem-email').value;
+    localStorage.setItem('temporalEmail', email);
+    window.location.href='register.html'
+}
