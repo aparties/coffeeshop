a428f65e07f38cb3b9d11dfc23e7e157aec8e9ad
Jaime Suarez <jaisua8@gmail.com>
2025-02-19 23:56:30 -0500

    actualizacion de la ventanas del pedido

--- codigo/pedido.js
+++ codigo/pedido.js
@@ -1,32 +1,26 @@
+//Menus de la ventanas principales
 const li= document.querySelectorAll('.li')
 const bloque=document.querySelectorAll('.bloque')
+
+//Menus internos de cada ventana
+const li2= document.querySelectorAll('.li2')
+const bloque2=document.querySelectorAll('.bloque2')
 
 //cuando hacemos click en un li
     //quita de todos los .li y .bloque la clase activo
     //.li y .bloque con la posicion se añadimos la clase activo
-
-li.forEach((cadaLi , i)=>{
-    li[i].addEventListener('click',()=>{
-        li.forEach((cadaLi,i)=>{
-            li[i].classList.remove('activo')
-            bloque[i].classList.remove('activo')
+function recorrerMenus(li,bloque,nameClass){
+    li.forEach((element , i)=>{
+        element.addEventListener('click',()=>{
+            li.forEach((element,i)=>{
+                element.classList.remove(nameClass);
+                bloque[i].classList.remove(nameClass);
+            })
+            element.classList.add(nameClass);
+            bloque[i].classList.add(nameClass);
         })
-        li[i].classList.add('activo')
-        bloque[i].classList.add('activo')
     })
-})
+}
 
-const li2= document.querySelectorAll('.li2')
-const bloque2=document.querySelectorAll('.bloque2')
-
-li2.forEach((cadaLi , i)=>{
-    li2[i].addEventListener('click',()=>{
-        li2.forEach((cadaLi,i)=>{
-            li2[i].classList.remove('activo2')
-            bloque2[i].classList.remove('activo2')
-        })
-
-        li2[i].classList.add('activo2')
-        bloque2[i].classList.add('activo2')
-    })
-})
+recorrerMenus(li, bloque, "activo")
+recorrerMenus(li2, bloque2, "activo2")
