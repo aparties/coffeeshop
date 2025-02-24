3c7a3598cf65a6eedb0dc9215a0e2a5a2e22ce72
Jaime Suarez <jaisua8@gmail.com>
2025-02-23 23:00:08 -0500

    actualizacion varios en pedidos tercera ventana 1.4

--- codigo/mediosPago.js
+++ codigo/mediosPago.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {   
     const body = document.querySelector('body')
     const div = document.createElement('div')
-    body.style.display = 'flex'
-    body.style.justifyContent = 'center'
-
+    const 
     div.classList.add('valor')
-    div.style.width ="200px"
-    div.style.textAlign = "center"
     body.appendChild(div)
 
 })
