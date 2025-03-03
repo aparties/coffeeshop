3a38e9c63cee3930a7d7bb829a508c2a52dd532e
Jaime Suarez <jaisua8@gmail.com>
2025-03-03 17:02:50 -0500

    listacarrritos

--- codigo/listaCarrito.js
+++ codigo/listaCarrito.js
@@ -7,13 +7,13 @@ document.addEventListener('DOMContentLoaded',() =>{
     let productos="";
     Object.keys(carrito).forEach(nombreProducto => productos+= `${nombreProducto}: ${carrito[nombreProducto]}<br>`);
     detalleCarrito.innerHTML = `<br>
-                    <h2 class="detalle-pago"><ion-icon name="cart-outline"></ion-icon> Detalle de carrito</h2><br>
-                    <h3 class="detalle-pago" style="font-weight:bold">${productos}</h3>
+                    <h2 class="detalle-pago"><ion-icon name="cart-outline" style="font-size:25px;"></ion-icon> Detalle de carrito</h2><br>
+                    <h3 class="detalle-pago" style="font-weight:bold;">${productos}</h3>
                     <br>
                     <h3 class="detalle-pago">Total a pagar:</h3>
                     <span class ="pago-Valor">S/. ${precioDeCompra}</span>
                     <br>
-                    <button type="button" class="btn" style="width: 80%" onclick="irCarrito()">Regresar al Carrito</button>`;
+                    <button type="button" class="btn" style="width: 80%;" onclick="irCarrito()">Regresar al Carrito</button>`;
 })
 
 const botonRet = document.querySelector("boton-return")
